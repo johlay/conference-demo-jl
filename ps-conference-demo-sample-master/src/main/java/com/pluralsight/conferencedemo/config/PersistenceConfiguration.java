e26dce2b3d5ecbee4d86f44e45e1c3a995483b82
Johnny Lay <86290365+johlay@users.noreply.github.com>
2022-06-30 15:58:58 +0200

    Add files via upload

---
+++ ps-conference-demo-sample-master/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,16 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class PersistenceConfiguration {
+    // @Bean
+    // public DataSource dataSource() {
+    //     DataSourceBuilder builder = DataSourceBuilder.create();
+    //     builder.url("jdbc:postgresql://localhost:5432/conference_app");
+    //     System.out.println("My custom datasource bean has been initialized and set");
+    //     return builder.build();
+    // }
+
+}
